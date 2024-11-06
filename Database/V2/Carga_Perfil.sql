4cc808cbf86d8b531f706908b7a6ddc74d61ef12
renatacometti <renatacometti2@gmail.com>
2024-11-05 21:31:25 -0300

    ajustes

---
+++ Database/V2/Carga_Perfil.sql
@@ -0,0 +1,6 @@
+/****** Script do comando SelectTopNRows de SSMS  ******/
+
+insert into [dbo].[Perfil]([nome], [Descricao], [Ativo])
+values
+('administrador', 'Administrador Sistema', 1),
+('cliente', 'Cliente e-comerce', 1);
---
+++ Database/V2/Carga_Perfil_Permissao.sql
@@ -0,0 +1,8 @@
+/****** Script do comando SelectTopNRows de SSMS  ******/
+insert into [dbo].[Perfil_Permissao]([Id_Perfil], [Id_Permissao])
+values
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5);
